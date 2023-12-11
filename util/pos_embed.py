2e60d7f67115864a598cb085862a5ae082a30293
Masood Mortazavi <masood.mortazavi@futurewei.com>
2023-12-11 13:17:00 -0800

    Corrected comment upon checking with Nabil.

--- util/pos_embed.py
+++ util/pos_embed.py
@@ -1,8 +1,6 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# Author: Nabil Ibtehaz (https://github.com/nibtehaz)
+# Insprired from https://github.com/facebookresearch/MAE
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 # --------------------------------------------------------
 # Position embedding utils
 # --------------------------------------------------------
